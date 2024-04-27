00ee6b7ab38011eb5bc2309ef54097f5c6180ee6
Ayush Singh <ayushsingh46026@gmal.com>
2024-04-27 14:19:49 +0530

    add testing code.

--- middlewares/CheckAuth.js
+++ middlewares/CheckAuth.js
@@ -4,6 +4,8 @@ const { verifyToken } = require("../services/AuthToken")
 // PENDINFG TO VERIFY USER TO RESTRICT ONY LOGIN PERSON CAN USE IT
 const CheckAuthentication = (req, res,next) => {
     try {
+        console.log("cookies:",req.cookies)
+        console.log("req",req.cookies)
         // pending
         const token = req.cookies.authToken
         if (!token) {
